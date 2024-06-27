a4216f0112d871acd48bd5f35525f3b87bd0f51e
javen.liu <javen.liu@greendotcorp.com>
2024-06-27 13:34:15 +0800

    update

--- src/index.js
+++ src/index.js
@@ -8,18 +8,7 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.liuhai.workers.dev": dockerHub,
-  //"docker.lixd.xyz": dockerHub,
-  //"quay.lixd.xyz": "https://quay.io",
-  //"gcr.lixd.xyz": "https://gcr.io",
-  //"k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  //"k8s.lixd.xyz": "https://registry.k8s.io",
-  //"ghcr.lixd.xyz": "https://ghcr.io",
-  //"cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
-
-  // staging
-  //"docker-staging.lixd.xyz": dockerHub,
+  "docker.liuhai.workers.dev": dockerHub
 };
 
 function routeByHosts(host) {
